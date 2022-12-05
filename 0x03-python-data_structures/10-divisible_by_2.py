a97fd5ca65be5d0d03656ca07598a7bed554296a
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2022-12-05 22:05:25 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
-gvbhdh svjkbhf,b vnvb,.vf
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    myList = []
+    for x in my_list:
+        if x % 2 is 0:
+            myList.append(True)
+        else:
+            myList.append(False)
+    return(myList)
