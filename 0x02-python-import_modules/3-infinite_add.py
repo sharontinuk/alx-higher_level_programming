2fbac8432dcd8d695db48346f8a8ac12e3a3081f
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2022-12-06 21:39:18 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
-ghflgbhvnhvggv kfngfnhvkjfh
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+lenght = len(sys.argv) - 1
+sum = 0
+if lenght == 0:
+    print("0")
+else:
+    for i in range(lenght):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
