b6f21b4b006632e2428e7bab32e053d21db7aa43
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-14 20:45:57 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,15 @@
-jjfklfleleejerjl
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
