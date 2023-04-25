0e59650a25e072c8fa277b0001c9513f90d7b6ae
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-04-25 22:02:01 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,3 +7,14 @@ request(url, function (error, response, body) {
     printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
